0081efe7952de4a871c060dcd97229135c1edf65
dlaxcess <flipiste@free.fr>
2025-01-31 15:59:58 +0100

    wip

--- src/lib/components/actionButton/index.js
+++ src/lib/components/actionButton/index.js
@@ -1,11 +1,24 @@
+// @ts-check
 import ActionButton from './ActionButton.svelte';
 
+/** @typedef {import('svelte').SvelteComponent} SvelteComponent */
+
 class ActionButtonElement extends HTMLElement {
+    /** @type {SvelteComponent|undefined} */
+    _element;
+
     constructor() {
         super();
-        new ActionButton({
+    }
+
+    connectedCallback() {
+        this._element = new ActionButton({
             target: this,
         });
+    }
+
+    disconnectedCallback() {
+        this._element?.destroy();
     }
 }
 
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,5 @@
-export * from './components/actionButton/index.js';
-export * from './components/counter/index.js';
-export * from './components/counterDisplay/index.js';
-export * from './components/notifier/index.js';
-export * from './components/securePinPad/index.js';
+export { default as ActionButton } from './components/actionButton/index.js';
+export { default as Counter } from './components/counter/index.js';
+export { default as CounterDisplay } from './components/counterDisplay/index.js';
+export { default as Notifier } from './components/notifier/index.js';
+export { default as SecurePinPad } from './components/securePinPad/index.js';
